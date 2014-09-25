45273366beb7b4de9a37c6143c8bdce3e61be2c0
Phil Barlit <pbarlit@gmail.com>
2014-09-25 15:09:55 -0400

    Week_4

--- Week_4/Hash_w_nested_arrays.rb
+++ Week_4/Hash_w_nested_arrays.rb
@@ -1,12 +1,1 @@
-def get_all_values_nested(nested_hash={}) 
-    nested_hash.each_pair do |k,v|
-      case v
-        when String, Fixnum then
-          @all_values << v
-        when Hash then get_all_values_nested(v)
-        else raise ArgumentError, "Unhandled type #{v.class}"
-      end
-    end
-     
-    return @all_values
-  end
+a = Array.new(8) {Array.new (8)}
